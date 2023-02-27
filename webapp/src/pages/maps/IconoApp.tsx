27cdec9c0e2097e82897b0b2853cf76c12e6e26f
michumier <miguelmierc@gmail.com>
2023-02-27 09:56:10 +0100

    Creacion boton y logo

---
+++ webapp/src/pages/maps/IconoApp.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Image(): JSX.Element{
+    return (
+        <>
+            <div className="imagen">
+                <img src="images/icono2.jpg" alt="Icono" />
+            </div>
+        </>
+    );
+}
+
+export default Image;
---
+++ webapp/src/pages/maps/InicioSesion.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+
+
+function InicioSesion(): JSX.Element{
+    return (
+        <>
+            <div className="inicioSesion">
+            <Button variant="outline-warning">Inicio de sesión</Button>{' '}
+            </div>
+        </>
+    );
+}
+
+export default InicioSesion;
