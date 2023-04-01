62dd7075d0abe18a4d6e5ebb4ae3800067e5320e
uo276026 <91556753+uo276026@users.noreply.github.com>
2023-04-01 16:00:50 +0200

    Modal listo

--- webapp/src/pages/maps/MapsPage.tsx
+++ webapp/src/pages/maps/MapsPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import NavigationMenu from "./components/NavigationMenu";
 import Filters from "./components/Filters";
-import Map from "./components/Map";
 import Info from "./components/Info";
+import Map from "./components/Map";
 import './MapsPage.css';
+import {FormGroup, Label, Modal, ModalBody, ModalHeader} from 'reactstrap';
 
+import { MapContainer, TileLayer, useMap, useMapEvents  } from 'react-leaflet';
+import {Marker, Popup} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+
+type MapProps = {
+    
+};
 
 function MapsPage():JSX.Element{
 
@@ -30,7 +39,7 @@ function MapsPage():JSX.Element{
 
                         {/*Mapa*/}
                         <div className="mapa">
-                            <Map/>
+                            <MapComponent/>
                         </div>
 
                         {/*Información*/}
@@ -39,6 +48,41 @@ function MapsPage():JSX.Element{
                         </div>
                     </div>
                 </div>  
+            </div>
+        </>
+    );
+}
+
+function MapComponent(props: MapProps): JSX.Element {
+    let modalOpened=false;
+    return (
+        <>
+            <div className="buscador">
+                <input type="text" name="buscar"></input>
+                <button> 🔍︎ Buscar  </button>
+            </div>
+            <div className="map">
+                <MapContainer center={[43.35485, -5.85123]} zoom={13} scrollWheelZoom={false} > 
+                    <TileLayer 
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Map/>
+                </MapContainer>
+            </div>
+            <div id="myModal" className="modal">    
+                <div className="modal-content">
+                <button id="closeModal" type="button" className="close" aria-label="Close">
+                    <span>&times;</span>
+                </button>
+                <form id="formAñadirLugar">
+                <p>Nombre del Lugar: <input id="nombreLugar" type="text"></input></p>
+                <p>Descripción: <input id="descrpLugar" type="text"></input></p>
+                <p>Comentario: <input id="comentLugar" type="text"></input></p>
+                </form>
+                <button id="añadirLugar" type="button" className="enviar"> Añadir lugar </button>
+                
+                </div>
             </div>
         </>
     );
--- webapp/src/pages/maps/components/Map.tsx
+++ webapp/src/pages/maps/components/Map.tsx
@@ -1,13 +1,14 @@
 import L from "leaflet";
-
 import { MapContainer, TileLayer, useMap, useMapEvents  } from 'react-leaflet';
 import {Marker, Popup} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { getPlaces } from '../../../api/api';
-import React,{useState,useEffect} from 'react';
+
 import { Place } from '../../../shared/shareddtypes';
 
-
+type MapProps = {
+    
+};
 
 const icon = new L.Icon({
     iconUrl: require('../../../assets/marker-icon.png'),
@@ -16,18 +17,15 @@ const icon = new L.Icon({
     className: 'leaflet-div-icon'
 });
 
-type MapProps = {
-    
-};
 
-
-let listPlaces:Place[] = [{
+//Luego se cambiaran por las de la base de datos
+let listPlaces:Place[] = [ {
     direction: "Calle Valdés Salas 11",
     latitude:43.35485,
     longitude:-5.85123,
     comments: "",
     photoLink:[]
-}, {
+    },{
      direction: "Gijón",
      latitude:43.5322,
      longitude:-5.6611,
@@ -36,11 +34,7 @@ let listPlaces:Place[] = [{
 }]
 
 
-
-
-function Map(props: MapProps): JSX.Element {
-
-    const defaultPlace:Place = {
+/* const defaultPlace:Place = {
         direction: "Aviles",
         latitude:43.5580,
         longitude:-5.9247,
@@ -58,57 +52,85 @@ function Map(props: MapProps): JSX.Element {
 
     let getMarkups = async () => {
         setMarkers(await getPlaces());
-    }
+    } */
    
 
-    return (
+function Map(props: MapProps): JSX.Element {
 
-        <>
-            <form name="lugares" onSubmit={handleClick}>
-                <button type="submit"> Cargar</button>
-            </form>
-            <div className="buscador">
-                <input type="text" name="buscar"></input>
-                <button> 🔍︎ Buscar  </button>
-            </div>
-            <div className="map">
-                <MapContainer center={[43.35485, -5.85123]} zoom={13} scrollWheelZoom={false} > 
-                    <TileLayer 
-                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                    <MapContent />
-                    {listPlaces.map((position2, idx) =>
-                        <Marker key={idx} position={[position2.latitude, position2.longitude]} icon={icon}>
-                            <Popup>
-                            {position2.direction}
-                        </Popup>
-                        
-                        </Marker>
-                    )} 
-                </MapContainer>
-            </div>
-        </>
-    );
-}
-
-
-const MapContent = () => {
     const map = useMapEvents({
-        click(e) {              
-            var marker = new L.Marker([e.latlng.lat,e.latlng.lng]);  
-            marker.setIcon(icon);
-            map.addLayer(marker);
+        click(e) {    
+            //abrir el modal
+            let modal = document.getElementById("myModal");
+            modal!.style.display = "block";
+            //cerrar el modal al hacer click en cruz
+            let botonCerrar = document.getElementById("closeModal");
+            if (botonCerrar != undefined) {
+                botonCerrar.onclick = function() {
+                    modal!.style.display = "none";}
+            }
+            //Mandar informacion y añadir el lugar
+            let botonAñadir = document.getElementById("añadirLugar");
+            if (botonAñadir != undefined) {
+                botonAñadir.onclick = function() {
+                    let nombreLugar = (document.getElementById("nombreLugar") as HTMLInputElement).value;
+                    let descrpLugar = (document.getElementById("descrpLugar") as HTMLInputElement).value;
+                    let comentLugar = (document.getElementById("comentLugar") as HTMLInputElement).value;
+                    if(nombreLugar!=""){
+                        modal!.style.display = "none";
+                        let element:Place = {
+                            direction: nombreLugar,
+                            latitude:e.latlng.lat,
+                            longitude:e.latlng.lng,
+                            comments: comentLugar,
+                            photoLink:[]
+                        }
+                        AddMarkerWithMap(element, map);
+                        reiniciarModal();
+                        
+                    }
+                } 
+            }
+
         },            
     })
 
-    return (
+    //Añadir markers de listPlaces (Luego se cambiara por los que esten en la base de datos o el pod)
+    for (const element of listPlaces) {
+        AddMarker(element);
+    }
 
-        <>   
-           
-        </>
-    );
+     return (
+            <>   
+               
+            </>
+        );
+    
 }
+
+
+function AddMarker(element:Place){
+    const map = useMapEvents({});
+    let marker = new L.Marker([element.latitude, element.longitude]);
+    marker.setIcon(icon);
+    map.addLayer(marker);
+    marker.bindPopup(element.direction)
+    marker.on('click', function(){ marker.openPopup() });
+}
+
+function AddMarkerWithMap(element:Place, map:L.Map){
+    let marker = new L.Marker([element.latitude, element.longitude]);
+    marker.setIcon(icon);
+    map.addLayer(marker);
+    marker.bindPopup(element.direction)
+    marker.on('click', function(){ marker.openPopup() });
+}
+
+function reiniciarModal(){
+    (document.getElementById("nombreLugar") as HTMLInputElement).value="";
+    (document.getElementById("descrpLugar") as HTMLInputElement).value="";
+    (document.getElementById("comentLugar") as HTMLInputElement).value="";
+}
+
 
 
 
