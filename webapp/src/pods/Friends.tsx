84c60142ea349cb8a0bf920f73d9c294f5486d89
UO282631 <UO282631@uniovi.es>
2023-04-16 02:39:41 +0200

    Primera versión amigos

---
+++ webapp/src/pods/Friends.tsx
@@ -0,0 +1,14 @@
+export type Friend = {
+    name : string,
+    webId : string,
+  }
+
+  export type Location = {
+    id?: string;
+    name: string,
+    category: string,
+    comments?: string,
+    latitud?: number,
+    longitud?: number,
+    score?: number
+}
