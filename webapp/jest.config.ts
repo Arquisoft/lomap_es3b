a611c2e9ba366a6b210cec372f4e264916d40327
UO271588 <UO271588@uniovi.es>
2023-04-29 12:56:25 +0200

    eliminacion de unas pruebas e2e

--- webapp/jest.config.ts
+++ webapp/jest.config.ts
@@ -5,12 +5,17 @@ import { defaults } from 'jest-config';
 const config: Config = {
     moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
     testEnvironment:"jsdom",
-    moduleNameMapper: {"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.js", "\\.(css|less)$": "<rootDir>/assetsTransformer.js"},
+    moduleNameMapper: {"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/assetsTransformer.ts", "\\.(css|less)$": "<rootDir>/assetsTransformer.ts"},
     transform: {
         "^.+\\.tsx?$":'ts-jest'
     },
     collectCoverage:true,
     collectCoverageFrom:["!**/__tests__/**", "**/*.tsx"],
+    transformIgnorePatterns: [
+        "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.js$",
+        "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.ts$",
+        "/node_modules/(?![@autofiy/autofiyable|@autofiy/property]).+\\.tsx$",
+    ]
 };
 
 export default config;
---
+++ webapp/src/pages/maps/components/InicioSesion.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import InicioSesion from './InicioSesion';
+import { useSession } from '@inrupt/solid-ui-react';
+
+
+
+// And in MyWrapper, you would override useSession with your mock:
+jest.mock("@inrupt/solid-ui-react", () => ({
+    useSession: () => ({
+        session: {
+            info: {
+                webId: "https://uo271588.inrupt.net/profile/card#me",
+                isLoggedIn: false,
+            },
+        },
+    })
+}));
+
+describe("LogIn button", () => {
+    test('Render Log In button', () => {
+
+        render(<InicioSesion />);
+        const boton = screen.getByText("Log In");
+        expect(boton).toBeInTheDocument();
+    });
+});
