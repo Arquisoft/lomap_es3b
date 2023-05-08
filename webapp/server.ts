9f468fb1289b55699456ace3488788f0413bc4b8
UO271588 <UO271588@uniovi.es>
2023-05-08 15:56:35 +0200

    Cambios para funcionar con https en despliegue

--- webapp/server.ts
+++ webapp/server.ts
@@ -1,4 +1,6 @@
-import express,{Application} from 'express'; 
+import express,{Application} from 'express';
+import https from "https";
+import fs from "fs";
 //for using an import here we need to configure the tsconfig.json
 //setting the option module to commonjs
 
@@ -9,8 +11,13 @@ const port: number = 3000;
 
 app.use(express.static('build'))
 
-app.listen(port, ():void => {
-    console.log('Webapp started on port '+ port);
+const options = {
+    key: fs.readFileSync('./certificate/private-key.pkey'),
+    cert: fs.readFileSync('./certificate/certificate.cert')
+}
+
+https.createServer(options,app).listen(port,():void=>{
+    console.log('(HTTPS) Webapp started on port '+ port);
 }).on("error",(error:Error)=>{
     console.error('Error occured: ' + error.message);
 });
